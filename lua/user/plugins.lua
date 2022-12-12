c16b874451d4ca20272df03e88c7d1f04e85b78d
sirfz <fayez@mantika.ca>
2022-12-12 13:36:34 +0000

    added polyglot

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,8 @@ return packer.startup(function(use)
     use { "EdenEast/nightfox.nvim" }
     use { "tomasiser/vim-code-dark" }
     use { "marko-cerovac/material.nvim" }
+    -- syntax
+    use { "sheerun/vim-polyglot" }
 
     -- cmp plugins
     use { "hrsh7th/nvim-cmp" } -- The completion plugin
