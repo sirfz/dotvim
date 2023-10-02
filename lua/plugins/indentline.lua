43dcb240f5d9913702b8f46f0684f4aa66e4bdd4
sirfz <fayez@mantika.ca>
2023-10-02 13:46:15 +0000

    updated indentline

--- lua/plugins/indentline.lua
+++ lua/plugins/indentline.lua
@@ -1,28 +1,35 @@
 local M = {
 	"lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
     event = "BufReadPre",
 	opts = {
-		char = "¦",
-		show_trailing_blankline_indent = false,
-		show_first_indent_level = false,
-		use_treesitter = true,
-		show_current_context = true,
-		buftype_exclude = {
-			"terminal",
-			"nofile",
-			"quickfix",
-			"prompt",
-		},
-		filetype_exclude = {
-			"lspinfo",
-			"packer",
-			"checkhealth",
-			"help",
-			"man",
-            "neo-tree",
-            "Trouble",
-			"",
-		},
+		indent = {
+            char = "¦",
+        },
+        whitespace = {
+            remove_blankline_trail = true
+        },
+        scope = {
+            enabled = true,
+        },
+		exclude = {
+            buftypes = {
+                "terminal",
+                "nofile",
+                "quickfix",
+                "prompt",
+            },
+            filetypes = {
+                "lspinfo",
+                "packer",
+                "checkhealth",
+                "help",
+                "man",
+                "neo-tree",
+                "Trouble",
+                "",
+            },
+        },
 	}
 }
 
