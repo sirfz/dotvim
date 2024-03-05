4e3586227f2cf041611e2e00a9aa8f96f86bcb37
sirfz <fayez@mantika.ca>
2024-03-05 13:36:43 +0000

    updated cmp order

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -11,137 +11,135 @@ local M = {
 }
 
 function M.config()
-  local check_backspace = function()
-      local col = vim.fn.col(".") - 1
-      return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-  end
+    local check_backspace = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+    end
 
-  local kind_icons = {
-      Text = "󰉿",
-      Method = "󰆧",
-      Function = "󰆧",
-      Constructor = "󰆧",
-      Field = "",
-      Variable = "",
-      Class = "󰌗",
-      Interface = "",
-      Module = "󰅩",
-      Property = "",
-      Unit = "",
-      Value = "󰎠",
-      Enum = "",
-      Keyword = "󰉨",
-      Snippet = "󰃐",
-      Color = "󰏘",
-      File = "󰈙",
-      Reference = "",
-      Folder = "󰉋",
-      EnumMember = "",
-      Constant = "󰇽",
-      Struct = "",
-      Event = "",
-      Operator = "󰆕",
-      TypeParameter = "󰊄",
-  }
+    local kind_icons = {
+        Text = "󰉿",
+        Method = "󰆧",
+        Function = "󰆧",
+        Constructor = "󰆧",
+        Field = "",
+        Variable = "",
+        Class = "󰌗",
+        Interface = "",
+        Module = "󰅩",
+        Property = "",
+        Unit = "",
+        Value = "󰎠",
+        Enum = "",
+        Keyword = "󰉨",
+        Snippet = "󰃐",
+        Color = "󰏘",
+        File = "󰈙",
+        Reference = "",
+        Folder = "󰉋",
+        EnumMember = "",
+        Constant = "󰇽",
+        Struct = "",
+        Event = "",
+        Operator = "󰆕",
+        TypeParameter = "󰊄",
+    }
 
-  local cmp = require("cmp")
-  local luasnip = require("luasnip")
-  require("luasnip/loaders/from_vscode").lazy_load()
+    local cmp = require("cmp")
+    local luasnip = require("luasnip")
+    require("luasnip/loaders/from_vscode").lazy_load()
 
-  cmp.setup({
-    snippet = {
-      expand = function(args)
-        luasnip.lsp_expand(args.body) -- For `luasnip` users.
-      end,
-    },
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                luasnip.lsp_expand(args.body) -- For `luasnip` users.
+            end,
+        },
 
-    mapping = cmp.mapping.preset.insert({
-      ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-      ["<C-f>"] = cmp.mapping.scroll_docs(4),
-      ["<C-Space>"] = cmp.mapping.complete(),
-      ["<C-e>"] = cmp.mapping.abort(),
-      -- Accept currently selected item. If none selected, `select` first item.
-      -- Set `select` to `false` to only confirm explicitly selected items.
-      ["<CR>"] = cmp.mapping.confirm({ select = false }),
-      ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-              cmp.select_next_item()
-          elseif luasnip.expandable() then
-              luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-              luasnip.expand_or_jump()
-          elseif check_backspace() then
-              fallback()
-          else
-              fallback()
-          end
-      end, {
-          "i",
-          "s",
-      }),
-      ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-              cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-              luasnip.jump(-1)
-          else
-              fallback()
-          end
-      end, {
-          "i",
-          "s",
-      }),
-    }),
-    formatting = {
-      fields = { "kind", "abbr", "menu" },
-      format = function(entry, vim_item)
-        vim_item.kind = kind_icons[vim_item.kind]
-        vim_item.menu = ({
-          nvim_lsp = "",
-          nvim_lua = "",
-          luasnip = "",
-          buffer = "",
-          path = "",
-          emoji = "",
-        })[entry.source.name]
-        return vim_item
-      end,
-    },
-    sources = cmp.config.sources({
-      { name = "nvim_lsp" },
-    }, {
-      {
-        name = "buffer",
-        option = {
-          get_bufnrs = function()
-            return vim.api.nvim_list_bufs()
-          end
-        }
-      },
-      { name = "path" },
-    }, {
-      { name = "nvim_lua" },
-      { name = "luasnip" },
-    }),
-    -- confirm_opts = {
-    --     behavior = cmp.ConfirmBehavior.Replace,
-    --     select = false,
-    -- },
-    window = {
-      completion = cmp.config.window.bordered(),
-      documentation = cmp.config.window.bordered(),
-    },
-    -- experimental = {
-    --     ghost_text = true,
-    -- },
-    cmp.setup.filetype('gitcommit', {
-      sources = cmp.config.sources({
-        { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-      }, {
-        { name = 'buffer' },
-      })
+        mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-Space>"] = cmp.mapping.complete(),
+            ["<C-e>"] = cmp.mapping.abort(),
+            -- Accept currently selected item. If none selected, `select` first item.
+            -- Set `select` to `false` to only confirm explicitly selected items.
+            ["<CR>"] = cmp.mapping.confirm({ select = false }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_next_item()
+                elseif luasnip.expandable() then
+                    luasnip.expand()
+                elseif luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                elseif check_backspace() then
+                    fallback()
+                else
+                    fallback()
+                end
+            end, {
+                "i",
+                "s",
+            }),
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+                if cmp.visible() then
+                    cmp.select_prev_item()
+                elseif luasnip.jumpable(-1) then
+                    luasnip.jump(-1)
+                else
+                    fallback()
+                end
+            end, {
+                "i",
+                "s",
+            }),
+        }),
+        formatting = {
+            fields = { "kind", "abbr", "menu" },
+            format = function(entry, vim_item)
+                vim_item.kind = kind_icons[vim_item.kind]
+                vim_item.menu = ({
+                    nvim_lsp = "",
+                    nvim_lua = "",
+                    luasnip = "",
+                    buffer = "",
+                    path = "",
+                    emoji = "",
+                })[entry.source.name]
+                return vim_item
+            end,
+        },
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "path" },
+            {
+                name = "buffer",
+                option = {
+                    get_bufnrs = function()
+                        return vim.api.nvim_list_bufs()
+                    end
+                }
+            },
+            { name = "luasnip" },
+            { name = "nvim_lua" },
+        }),
+        -- confirm_opts = {
+        --     behavior = cmp.ConfirmBehavior.Replace,
+        --     select = false,
+        -- },
+        window = {
+            completion = cmp.config.window.bordered(),
+            documentation = cmp.config.window.bordered(),
+        },
+        -- experimental = {
+        --     ghost_text = true,
+        -- },
+        cmp.setup.filetype('gitcommit', {
+            sources = cmp.config.sources({
+                { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+            }, {
+                { name = 'buffer' },
+            })
+        })
     })
-  })
 end
 
 return M
