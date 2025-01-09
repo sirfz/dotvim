bac8776ddaf593192a0810546003f31b5fe08d0b
sirfz <fayez@mantika.ca>
2025-01-09 15:03:28 +0000

    copilot config update

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,6 +1,7 @@
 return {
     {
         "zbirenbaum/copilot.lua",
+        enabled = false,
         config = true,
         cmd = "Copilot",
         event = "InsertEnter",
@@ -8,23 +9,20 @@ return {
             suggestion = { enabled = false },
             panel = { enabled = false },
         },
-    },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        cmd = "CopilotChat",
-        branch = "main",
         dependencies = {
-            "nvim-lua/plenary.nvim", -- for curl, log wrapper
-        },
-        config = true,
-        dependencies = {
-            "zbirenbaum/copilot.lua",
+            -- {
+            --     "CopilotC-Nvim/CopilotChat.nvim",
+            --     cmd = "CopilotChat",
+            --     branch = "main",
+            --     dependencies = {
+            --         "nvim-lua/plenary.nvim", -- for curl, log wrapper
+            --     },
+            --     config = true,
+            -- },
+            {
+                "giuxtaposition/blink-cmp-copilot",
+            },
+
         }
-    },
-    {
-        "giuxtaposition/blink-cmp-copilot",
-        dependencies = {
-            "zbirenbaum/copilot.lua",
-        },
     },
 }
