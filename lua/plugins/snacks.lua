92f7ab91a682b68c2805c7831d297b9bb6ba7f1f
sirfz <fayez@mantika.ca>
2025-08-07 10:11:13 +0000

    smart.picker config

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -3,6 +3,14 @@ return {
     ---@type snacks.Config
     opts = {
         picker = {
+            sources = {
+                files = {
+                    cmd = "fd",
+                },
+                grep = {
+                    cmd = "rg",
+                }
+            },
             matcher = {
                 fuzzy = true, -- use fuzzy matching
                 smartcase = true, -- use smartcase
@@ -16,10 +24,19 @@ return {
                 frecency = true, -- frecency bonus
                 history_bonus = false, -- give more weight to chronological order
             },
+            formatters = {
+                file = {
+                    filename_first = false, -- display filename before the file path
+                    truncate = 80, -- truncate the file path to (roughly) this length
+                    filename_only = false, -- only show the filename
+                    icon_width = 2, -- width of the icon (in characters)
+                    git_status_hl = true, -- use the git status highlight group for the filename
+                },
+            },
             win = {
                 input = {
                     keys = {
-                        ["<ESC>"] = { "cancel", mode = "i" },
+                        ["<ESC>"] = { "close", mode = { "n", "i" } },
                     }
                 }
             },
