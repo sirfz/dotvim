7c560ed6d76b0cb96a1b4879ddc83711d0485bfe
sirfz <iamfayez@gmail.com>
2025-08-21 22:56:11 +0300

    switched back to fzf-lua with frecency config update

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -1,8 +1,10 @@
 return {
     {
         'ibhagwan/fzf-lua',
-        enabled = false,
-        dependencies = { 'nvim-tree/nvim-web-devicons' },
+        enabled = true,
+        dependencies = {
+            'nvim-tree/nvim-web-devicons',
+        },
         branch = 'main',
         cmd = { 'FzfLua' },
         opts = {
@@ -12,11 +14,43 @@ return {
             helptags = { previewer = "help_native" },
             tags = { previewer = "bat" },
             btags = { previewer = "bat" },
-        }
+        },
+        keys = {
+            -- { "<leader>ff", ":FzfLua files<CR>", desc = "Smart Find Files" },
+            {
+                "<leader>ff", function()
+                    require('fzf-lua-frecency').frecency(
+                        {
+                            cwd_only=true,
+                            all_files=true,
+                            fzf_opts={["--no-sort"] = false }
+                        }
+                    )
+                end,
+                desc = "Files"
+            },
+            { "<leader>ft", ":FzfLua live_grep<CR>", desc = "Grep" },
+            {
+                "<leader>fm", function()
+                    require('fzf-lua-frecency').frecency(
+                        {
+                            cwd_only=false,
+                            all_files=true,
+                            fzf_opts={["--no-sort"] = false }
+                        }
+                    )
+                end,
+                desc = "Recent"
+            },
+            { "<leader>fb", ":FzfLua buffers<CR>", desc = "Buffers" },
+            { "<leader>fr", ":FzfLua resume<CR>", desc = "Resume" },
+            { "<leader>fk", ":FzfLua keymaps<CR>", desc = "Resume" },
+            { "<leader>fd", ":FzfLua diagnostics_document<CR>", desc = "Resume" },
+        },
     },
     {
         'elanmed/fzf-lua-frecency.nvim',
-        enabled = false,
+        enabled = true,
         dependencies = 'ibhagwan/fzf-lua',
         config = true,
     }
